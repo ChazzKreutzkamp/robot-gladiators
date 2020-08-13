f870db2f8b4475900cbb45658307518013f86cec
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-08-13 16:44:20 -0600

    MVP has been finished with a startGame and endGame function.

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,8 +64,51 @@ var fight = function(enemyName) {
       }
     }
   };
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);   
+  //function to start a new game
+var startGame = function() {
+  
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
+    
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+    
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+
+    }
+
+    else {
+      
+      window.alert("You have lost your robot in battle! GAME OVER!");
+      break;
+
+    }
+
+  }
+  endGame();
+};
+
+var endGame = function() {
+  window.alert("The game has now ended. Let's see hou you did!");
+  if (playerHealth > 0){
+  window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
 }
+else {
+  window.alert("You've lost your rbot in battle.");
+}
+var playAgainConfirm = window.confirm("Would you like to play again?");
+if (playAgainConfirm) {
+  startGame();
+}
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+};
+startGame();
