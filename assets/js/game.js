87f208f5aad07081f665ddc04b7235cc625be8b6
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-08-15 21:25:40 -0600

    The name bug has been fixed by adding a getName function.

--- assets/js/game.js
+++ assets/js/game.js
@@ -62,7 +62,6 @@ var startGame = function() {
     
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i+1));
-    
       var pickedEnemyObj = enemyInfo[i];
 
       pickedEnemyObj.health = randomNumber(40, 60);
@@ -130,8 +129,18 @@ var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max-min +1) + min);
   return value;
 };
+
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is" + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -161,7 +170,6 @@ var playerInfo = {
       }
     }
 };
-
 
 var enemyInfo = [
   {
